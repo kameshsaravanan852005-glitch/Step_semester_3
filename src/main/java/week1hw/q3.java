51595f7b026df7c3e039c0ad09eed8e1bf9d50ec
Kamesh Saravanan <YOUR_GITHUB_EMAIL>
2026-08-30 17:46:43 +0530

    Add week01hw question 04

--- src/main/java/week1hw/q3.java
+++ src/main/java/week1hw/q3.java
@@ -1,5 +1,58 @@
-package main.java.week1hw;
+import java.util.Scanner;
 
 public class q3 {
-    
+
+    static void findLongestStreak(String signalLog) {
+
+        if (signalLog.length() == 0) {
+            System.out.println("No signal data");
+            return;
+        }
+
+        char longestSignal = signalLog.charAt(0);
+        int longestLength = 1;
+
+        char currentSignal = signalLog.charAt(0);
+        int currentLength = 1;
+
+        for (int i = 1; i < signalLog.length(); i++) {
+
+            if (signalLog.charAt(i) == currentSignal) {
+                currentLength++;
+            } else {
+
+                if (currentLength > longestLength) {
+                    longestLength = currentLength;
+                    longestSignal = currentSignal;
+                }
+
+                currentSignal = signalLog.charAt(i);
+                currentLength = 1;
+            }
+        }
+
+        // Check the last streak
+        if (currentLength > longestLength) {
+            longestLength = currentLength;
+            longestSignal = currentSignal;
+        }
+
+        System.out.println("Longest Streak: '"
+                + longestSignal
+                + "' repeated "
+                + longestLength
+                + " times");
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter signal sequence: ");
+        String signalLog = sc.nextLine();
+
+        findLongestStreak(signalLog);
+
+        sc.close();
+    }
 }
